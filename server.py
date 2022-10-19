900b119181e2ecf054bc28fe5b9a0ef1cd5ba692
Chino Franco <chino.franco@gmail.com>
2022-10-19 22:57:09 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import socket
 import argparse
 from webserver import Server
 
@@ -7,19 +8,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--server", "-s",
                     type=str, required=False, 
                     help="IP address to listen on", 
-                    default="192.168.11.11")
+                    default=socket.gethostbyname((socket.gethostname()))
 parser.add_argument("--port",
                     type=int, required=False, 
                     help="Port to connect to", 
-                    default=5050)
+                    default=0)
 args = parser.parse_args()
 
 
 if __name__ == "__main__":
-    # Server configuration
-    SERVER_IP = "192.168.11.11"
-    PORT = 5050
-
     # Run instance
     test_server = Server(host=args.server, port=args.port)
     test_server.run()
