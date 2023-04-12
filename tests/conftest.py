c2f76efbf78cb08e88adcefab6a2669e315e4d9a
jgfranco17 <chino.franco@gmail.com>
2023-04-13 01:01:53 +0900

    Added fixtures for client and server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,18 @@
-import sys
 import pytest
+import socket
+from webserver.server import Server
 
 
-# each test runs on cwd to its temp dir
-@pytest.fixture(autouse=True)
-def go_to_tmpdir(request):
-    # Get the fixture dynamically by its name.
-    tmpdir = request.getfixturevalue("tmpdir")
-    # ensure local test created packages can be imported
-    sys.path.insert(0, str(tmpdir))
-    # Chdir only for the duration of the test.
-    with tmpdir.as_cwd():
-        yield
+@pytest.fixture
+def client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost', 8000))
+    yield client
+    client.close()
+    
+    
+@pytest.fixture(scope='module')
+def server():
+    server = Server(host="localhost", port=8000)
+    yield server
+    server.stop()
