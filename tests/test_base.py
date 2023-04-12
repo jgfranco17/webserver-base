41d148b5b9f447c4e476cb6b7ed54508a1311b84
jgfranco17 <chino.franco@gmail.com>
2023-04-13 01:02:06 +0900

    Added initial socket tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,21 @@
+import pytest
 from webserver.base import NAME
+from .conftest import server, client
 
 
-def test_base():
-    assert NAME == "project_template"
+def test_connection(client):
+    assert client.fileno() > 0
+
+def test_send_message(client):
+    message = b'Hello, world!'
+    client.sendall(message)
+    assert client.recv(1024) == message
+
+def test_invalid_message(client):
+    with pytest.raises(ValueError):
+        client.sendall(None)
+
+def test_disconnect(client):
+    assert client.fileno() > 0
+    client.close()
+    assert client.fileno() < 0
