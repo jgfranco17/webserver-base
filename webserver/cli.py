fb0cdfb84e072371e14d3a6d6305667f49d85c2f
jgfranco17 <chino.franco@gmail.com>
2023-04-12 22:08:16 +0900

    Added CLI flags and main run

--- webserver/cli.py
+++ webserver/cli.py
@@ -1,28 +1,33 @@
-"""CLI interface for project_template project.
-
-Be creative! do whatever you want!
-
-- Install click or typer and create a CLI app
-- Use builtin argparse
-- Start a web application
-- Import things from your .base module
 """
+CLI interface for webserver project.
+"""
+import argparse
+from .server import Server
+from .client import Client
 
 
 def main():  # pragma: no cover
     """
     The main function executes on commands:
     `python -m project_template` and `$ project_template `.
-
-    This is your program's entry point.
-
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
     """
-    print("This will do something")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", "-m",
+                        type=str, required=True,
+                        help="Set to server or client behavior")
+    parser.add_argument("--address", "-a",
+                        type=str, required=True,
+                        help="Host IP to bind to")
+    parser.add_argument("--port", "-p",
+                        type=str, required=True,
+                        help="Target port to bind to")
+    args = parser.parse_args()
+    
+    mode = args.mode.lower()
+    if mode not in ("server", "client"):
+        raise ValueError(f'Invalid mode: \"{mode}\" not recognized.')
+    
+    print(f'Running webserver in {mode} mode.')
+    if mode == "server":
+        server = Server(host=args.address, port=args.port)
+        server.run()
